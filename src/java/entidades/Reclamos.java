3c1bf9250dd2fb3ca3329617f488e8ca0f0dfb3a
Usuario <Usuario@A5-M07>
2019-05-21 18:01:27 -0300

    resp para el navegador: "Recibimos su pedido, aguarde sentado"

---
+++ src/java/entidades/Reclamos.java
@@ -0,0 +1,8 @@
+package entidades;
+
+public class Reclamos {
+    public String nombre;
+    public String email;
+    public String texto;
+    
+}
--- src/java/web/ReclamoServlet.java
+++ src/java/web/ReclamoServlet.java
@@ -1,25 +1,34 @@
 package web;
 
+import entidades.Reclamos;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet(name = "Reclamo", urlPatterns = {"/Reclamo"})
-public class Reclamo extends HttpServlet {
-
+public class ReclamoServlet extends HttpServlet {
+    
+  
+    
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Estamos en el metodo POST");
-                
+        
+        Reclamos miReclamo = new Reclamos();
+        miReclamo.nombre = "pepe";
+        miReclamo.email = "pepe@gmail.com";      
+        miReclamo.texto = "Los auriculares no funcionan";     
+        
+        System.out.println("miReclamo");
+        
+        
+        
         System.out.println(req.getReader().readLine());
+        resp.getWriter().print("Recibimos su pedido, aguarde sentado.");
         
     }
-
     
-   
 }
